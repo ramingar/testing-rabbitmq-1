f90ceabf41c024e6b60538f478c018d0627e5123
Rafael Minguet <rafael.minguet@martico.com>
2023-02-25 14:21:47 +0100

    Hello World sender and receiver

---
+++ src/config/config.dist.js
@@ -0,0 +1,4 @@
+export default {
+    rabbitMQUrl      : 'amqp://localhost',
+    rabbitMQQueueName: 'queue1'
+};
--- src/receive.js
+++ src/receive.js
@@ -1,5 +1,10 @@
 import amqp from 'amqplib/callback_api'
-import config from './config/config'
+import config from "./config/config";
+
+const queueConsumer = (msg) => {
+    console.log(`[x] Received: ${msg.content.toString()}`)
+}
+
 
 amqp.connect(config.rabbitMQUrl, (error0, connection) => {
     if (error0) throw error0
@@ -7,14 +12,10 @@ amqp.connect(config.rabbitMQUrl, (error0, connection) => {
         if (error1) throw error1
 
         const queue = config.rabbitMQQueueName;
-        const msg   = 'Hello World!!';
 
         channel.assertQueue(queue, {durable: false});
-        channel.sendToQueue(queue, Buffer.from(msg));
+        console.log(`[*] Waiting for messages in ${queue}. To exit press CTRL+C`);
 
-        setTimeout(() => {
-            connection.close();
-            process.exit(0)
-        }, 500);
+        channel.consume(queue, queueConsumer, {noAck: true})
     })
 })
